6159fcc4e16645fa20ee4c3617650116778e8f11
wsbky <wsbkye@gmail.com>
2021-03-23 17:09:57 +0900

    test

---
+++ main.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# install homebrew
+echo "Installing Homebrew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install Zsh
+brew install zsh
